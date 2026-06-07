137f4b925a669796ae8057e431d43cac4eef5aa3
sanskritisingh245 <sanskritisinghh2005@gmail.com>
2026-06-08 00:23:30 +0530

    wsAdded

---
+++ apps/ws-server/index.ts
@@ -0,0 +1,54 @@
+import { createClient } from "redis";
+
+const client = createClient();
+await client.connect();
+
+try{
+    await client.xGroupCreate("fills", "ws-group", "0",
+        {
+            MKSTREAM:true
+        }
+    )
+}catch(e){
+    console.log(e);
+}
+const server = Bun.serve({
+    port:8080,
+    fetch(req, server){
+        if(server.upgrade(req)) return;
+        return new Response("ws-server up");
+    },
+    websocket:{
+        open(ws){
+            ws.subscribe("fills");
+            console.log("client connected");
+        },
+        close(ws){
+            ws.unsubscribe("fills");
+        },
+        message(ws, msg) {},
+    }
+});
+
+while(true){
+    const response = await client.xReadGroup("ws-group", "ws-1",
+        [
+            {
+                key:"fills",
+                id: ">"
+            }
+        ],
+        {
+            BLOCK :0,
+            COUNT:10
+        },
+    )
+    if(!response) continue;
+
+    for(const stream of response) {
+        for(const message of stream.messages){
+            server.publish("fills", JSON.stringify(message.message));
+            await client.xAck("fills", "ws-group" , message.id);
+        }
+    }
+}
